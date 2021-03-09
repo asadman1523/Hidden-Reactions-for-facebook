a086da55e3e4ac86d406a7e72b2a8a4500f53ea1
Jack_Wu <dm3352andy@gmail.com>
2021-03-10 02:38:01 +0800

    Update popup options

--- background.js
+++ background.js
@@ -1,17 +1,29 @@
-//監聽facebook往下loading
-document.addEventListener("DOMNodeInserted", function(e) {
+var option = 1;
+chrome.storage.sync.get({
+	option: 1,
+}, function(items) {
+	option = items.option
+});
+document.addEventListener("DOMNodeInserted", function (e) {
 	removeReaction();
 }, false);
 
+/**
+ * <option value="0">Hide all reactions</option>
+ * <option value="1">Show the most popular reaction</option> 
+ */
 function removeReaction() {
+	if (option == 2) {
+		return;
+	}
+	var remainIcon = 1;
+	if (option == 0) {
+		remainIcon = 0;
+	}
 	var array = document.querySelectorAll("[role=toolbar]");
-	for (var i = 0;i<array.length;i++) {
-		while(array[i].firstElementChild.childElementCount>1) {
-			array[i].firstElementChild.children[1].remove();
+	for (var i = 0; i < array.length; i++) {
+		while (array[i].firstElementChild.childElementCount > remainIcon) {
+			array[i].firstElementChild.children[remainIcon].remove();
 		}
 	}
 }
-
-
-
-
