7c8c3aaf6fee41299cb97ff90b04b806faa89f3c
Jack_Wu <dm3352andy@gmail.com>
2024-09-24 16:10:50 +0800

    fix:替換棄用的DOMInsert

--- background.js
+++ background.js
@@ -4,9 +4,28 @@ chrome.storage.sync.get({
 }, function(items) {
 	option = items.option
 });
-document.addEventListener("DOMNodeInserted", function (e) {
-	removeReaction();
-}, false);
+
+// 配置觀察選項
+const config = { childList: true, subtree: true };
+
+// 創建一個MutationObserver實例，並傳入回調函數
+const observer = new MutationObserver((mutationsList, observer) => {
+    for (let mutation of mutationsList) {
+		// console.log('QQAQ');
+		// console.log(mutation.type);
+        if (mutation.type === 'childList') {
+            // console.log('A child node has been added or removed.');
+            // 在這裡處理元素插入的邏輯
+			removeReaction();
+        }
+    }
+});
+
+observer.observe(document, config);
+// document.addEventListener("DOMNodeInserted", function (e) {
+// 	console.log("insert")
+// 	removeReaction();
+// }, false);
 
 /**
  * <option value="0">Hide all reactions</option>
